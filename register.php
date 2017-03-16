0de6f49fd8be3946bd61c3f8d30163b285748d06
sanddc318 <sanddc318@gmail.com>
2017-03-16 16:07:00 -0400

    Base register form complete.

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Swirlfeed | Sign Up</title>
+</head>
+<body>
+  <form action="register.php" method="POST">
+    <input type="text" name="reg_fname" placeholder="First Name" required>
+    <br>
+    <input type="text" name="reg_lname" placeholder="Last Name" required>
+    <br>
+    <input type="email" name="reg_email" placeholder="Email" required>
+    <br>
+    <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+    <br>
+    <input type="password" name="reg_password" placeholder="Password" required>
+    <br>
+    <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+    <br>
+    <input type="submit" name="reg_button" value="Register" required>
+  </form>
+
+
+</body>
+</html>
