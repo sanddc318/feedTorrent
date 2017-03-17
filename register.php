aab219c38b4fbbebcdc29da39ff197dfbd5fe432
sanddc318 <sanddc318@gmail.com>
2017-03-16 20:52:36 -0400

    One ugly login form ready to go.

--- register.php
+++ register.php
@@ -9,6 +9,18 @@
   <title>Swirlfeed | Sign Up</title>
 </head>
 <body>
+  <!-- Login Form -->
+  <form action="register.php" method="POST">
+    <input type="email" name="log_email" placeholder="Email Address">
+    <br>
+    <input type="password" name="log_password" placeholder="Password">
+    <br>
+    <input type="submit" name="login_button" value="Login">
+  </form>
+
+  <hr>
+
+  <!-- Register Form -->
   <form action="register.php" method="POST">
     <!-- First name -->
     <input type="text" name="reg_fname" placeholder="First Name"
