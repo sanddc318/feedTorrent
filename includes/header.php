aa0b5fdf1e9fce4ab6ac70bc1617ff6b99000376
sanddc318 <sanddc318@gmail.com>
2017-03-25 23:30:14 -0400

    User detail column done.

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@
 
   if ( isset($_SESSION["username"]) ) {
     $loggedInUser = $_SESSION["username"];
+    $user_details_query = mysqli_query( $con, "SELECT * FROM users WHERE username = '$loggedInUser'" );
+    $user = mysqli_fetch_array( $user_details_query );
   } else {
     header( "Location: register.php" );
   }
@@ -15,6 +17,8 @@
   <title>Welcome to Swirlfeed</title>
   <!-- Googlefonts -->
   <link href="https://fonts.googleapis.com/css?family=Merienda+One" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- CSS -->
   <link rel="stylesheet" href="assets/css/bootstrap.css">
   <link rel="stylesheet" href="assets/css/styles.css">
@@ -28,4 +32,15 @@
     <div class="logo">
       <a href="index.php">Swirlfeed!</a>
     </div>
+
+    <nav>
+      <a href="#" class="user"><?php echo $user["first_name"]; ?></a>
+      <a href="index.php" class="nav-link"><i class="fa fa-home" aria-hidden="true"></i></a>
+      <a href="#" class="nav-link"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+      <a href="#" class="nav-link"><i class="fa fa-cog" aria-hidden="true"></i></a>
+      <a href="#" class="nav-link"><i class="fa fa-bell" aria-hidden="true"></i></a>
+      <a href="#" class="nav-link"><i class="fa fa-users" aria-hidden="true"></i></a>
+    </nav>
   </div>
+
+  <div class="wrapper">
--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php include("includes/header.php"); ?>
 
-  <span>Home page content</span>
+  <div class="user-details column">
+    <a href="#"><img src="<?php echo $user["profile_pic"] ?>"></a>
+    <div class="user-details-leftcol-right">
+      <span class="name">
+        <a href="#"><?php echo $user["first_name"] . " " . $user["last_name"] . "<br>"; ?></a>
+      </span>
+      <?php
+        echo "<em>#" . $user["username"] . "</em> <br><br>";
+        echo "Posts: " . $user["num_posts"] . "<br>";
+        echo "Likes: " . $user["num_likes"] . "<br>";
+        echo "Friends: 0" . "<br>";
+      ?>
+    </div>
+  </div>
+
+  </div> <!-- /.wrapper (header.php) -->
 
 </body>
 </html>
