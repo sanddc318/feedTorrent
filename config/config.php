679f67db8593747b1c102f87dd75d76140cf54d7
sanddc318 <sanddc318@gmail.com>
2017-05-22 20:30:01 -0400

    New database connection values.

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 
   $timezone = date_default_timezone_set( "America/New_York" );
 
-  $con = mysqli_connect( "localhost", "root", "root", "social" );
+  $con = mysqli_connect( "us-cdbr-iron-east-03.cleardb.net", "b0044335d43a7d", "55e34de1", "heroku_26caad6e80323fa" );
 
   if ( mysqli_connect_errno() ) {
     echo "Failed to connect: " . mysqli_connect_errno() . "<br>";
